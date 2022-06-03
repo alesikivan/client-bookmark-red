f9335c8baa8f66001fbe19c3e251c67fafc31251
alesikivan <obcofficiall@gmail.com>
2022-06-03 15:00:44 +0300

    fix(index.js): changed paths

--- core/index.js
+++ core/index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, '..', 'angular-bookmark')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'angular-bookmark', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 })
 
 const PORT = process.env.PORT || 3000
